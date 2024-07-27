096bac87715b3561360f567373e0e2b8168046de
zinuu4 <unvm7721@gmail.com>
2024-07-27 11:49:18 +0200

    chapter 4: creating layouts and pages

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+export default function CustomersPage() {
+  return (
+    <div>
+      <h1>Customers</h1>
+    </div>
+  );
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+export default function InvoicesPage() {
+  return (
+    <div>
+      <h1>Invoices</h1>
+    </div>
+  );
+}
